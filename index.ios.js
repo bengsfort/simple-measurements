393b2a33f28daa4b450934764b5a0296ba28e331
Matt Bengston <mattb@unity3d.com>
2018-01-19 09:52:12 +0200

    Fix linting issues

--- index.ios.js
+++ index.ios.js
@@ -4,41 +4,15 @@
  * @flow
  */
 
-import React, { Component } from 'react';
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+import React from 'react';
+import { AppRegistry } from 'react-native';
 
 import App from 'App';
 
-export default class EasyMeasurements extends Component {
-  render() {
-    return (
-      <App />
-    );
-  }
+export default function EasyMeasurements() {
+  return (
+    <App />
+  );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
 
 AppRegistry.registerComponent('EasyMeasurements', () => EasyMeasurements);
