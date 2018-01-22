c8acd79ed65b027090bc9a7bb6e7c510f5bb2c4f
Matt Bengston <mattb@unity3d.com>
2018-01-22 22:53:26 +0200

    Upgraded RN and renamed app

--- android/app/src/main/java/com/simpleconversions/MainApplication.java
+++ android/app/src/main/java/com/simpleconversions/MainApplication.java
@@ -1,4 +1,4 @@
-package com.easymeasurements;
+package com.simpleconversions;
 
 import android.app.Application;
 
@@ -24,6 +24,11 @@ public class MainApplication extends Application implements ReactApplication {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage()
       );
+    }
+
+    @Override
+    protected String getJSMainModuleName() {
+      return "index";
     }
   };
 
