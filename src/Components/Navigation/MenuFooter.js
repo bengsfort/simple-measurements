9a5d654ea644000b9f827209d2b5612b13b311e5
Matt Bengston <mattb@unity3d.com>
2018-04-02 17:19:39 +0300

    Menu footer inlined.

--- src/Components/Navigation/MenuFooter.js
+++ src/Components/Navigation/MenuFooter.js
@@ -15,12 +15,12 @@ import AnimatedHeart from './AnimatedHeart';
 export default function MenuFooter() {
   return (
     <View style={styles.container}>
-      {[
-      (<Text style={styles.content} key={0}>Made with </Text>),
-      (<AnimatedHeart key={1} />),
-      (<Text style={styles.content} key={2}> by </Text>),
-      (<Text style={[styles.content, styles.strong]} key={3}>@bengsfort</Text>),
-      ]}
+      <View style={styles.topRow}>
+        <Text style={styles.content}>Made with </Text>
+        <AnimatedHeart />
+        <Text style={styles.content}> by </Text>
+        <Text style={[styles.content, styles.strong]}>@bengsfort</Text>
+      </View>
       <Text style={styles.content}>in Helsinki, Finland</Text>
     </View>
   );
@@ -32,6 +32,11 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     height: 50,
     backgroundColor: ColoursStyle.black[100],
+  },
+  topRow: {
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
   },
   content: {
     fontSize: 14,
